a600c3a906f65d2bf7fe235d786202b897b4e6c3
Harmonigatr <43183515+Harmonigatr@users.noreply.github.com>
2019-01-26 16:02:09 -0500

    Kid Walking
    Added some Kid who walks. He also flips to face the direction he's walking in.

--- What Home Means to You/Assets/Scripts/Player/Player.cs
+++ What Home Means to You/Assets/Scripts/Player/Player.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-
+    private SpriteRenderer SprtRndrr;
     public float speed,
                   groundCheckRadious;
     public LayerMask isGroundedLayer;
     private Rigidbody2D rb2d;
     private float jumpHeight = 5,
                   movement;
-    private bool isGrounded;
+    private bool isGrounded,
+                 toRight = true;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        SprtRndrr = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -30,6 +32,10 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
             rb2d.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
 
+        if ((rb2d.velocity.x < 0 && toRight)||(rb2d.velocity.x > 0 && !toRight)) { 
+            toRight = !toRight;
+            SprtRndrr.flipX = !SprtRndrr.flipX;
+        }
     }
 }
 
