c43ee7df2354cca50b63556a6ecb9e29f5f36608
Harmonigatr <43183515+Harmonigatr@users.noreply.github.com>
2019-01-27 11:07:40 -0500

    Levelextendor script modified

--- What Home Means to You/Assets/Scripts/levelextender.cs
+++ What Home Means to You/Assets/Scripts/levelextender.cs
@@ -7,16 +7,11 @@ public class levelextender : MonoBehaviour
     public int[] order;
     public GameObject[] sets;
     public Transform genPoint;
-    private GameObject clone;
     private float spawn = 0.0f;
-    private int counter = 0;
-
+    private int counter = 1;
     private void Start()
     {
-        clone = Instantiate(sets[0]) as GameObject;
-        clone.transform.SetParent(transform);
     }
-
     private void Update()
     {
         /*if (clone.transform.position.x < genPoint.position.x  && counter < order.Length){
@@ -25,12 +20,18 @@ public class levelextender : MonoBehaviour
             clone.transform.position = new Vector2(Vector2.right.x * spawn, transform.position.y);
             spawn += sets[#].GetComponent<BoxCollider2D>().size.x;
         }*/
-        if (counter < order.Length)
+
+
+        if (counter < order.Length - 1)
         {
-            clone = Instantiate(sets[order[counter]]) as GameObject;
+            GameObject clone = Instantiate(sets[order[counter]]) as GameObject;
+            Transform enddif = sets[order[counter - 1]].transform.Find("end"),
+                      startdif = clone.transform.Find("start");
+            float lol = (enddif.position.y - sets[order[counter - 1]].transform.position.y);
+            float lo1 = (startdif.transform.position.y - clone.transform.position.y);
             clone.transform.SetParent(transform);
-            clone.transform.position = new Vector2(Vector2.right.x * spawn, transform.position.y);
-            spawn += sets[order[counter++]].GetComponent<BoxCollider2D>().size.x;
+            clone.transform.position = new Vector2(Vector2.right.x * spawn, lol - lo1);// y = genPoint.position.y
+            spawn += (sets[order[counter]].GetComponent<BoxCollider2D>().size.x * sets[order[counter++]].transform.lossyScale.x) / 2 + (sets[order[counter]].GetComponent<BoxCollider2D>().size.x * sets[order[counter]].transform.lossyScale.x) / 2;
         }
     }
 }
