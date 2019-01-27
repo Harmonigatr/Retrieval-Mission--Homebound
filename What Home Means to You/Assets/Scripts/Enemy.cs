5dcbe1b3ffa7de5d607e800ffb8de22cf0657e9d
Harmonigatr <43183515+Harmonigatr@users.noreply.github.com>
2019-01-27 03:53:36 -0500

    Organized Image and Pre Fabulus
    Added a Stuff folder for stability.
    Added some ai to enemy script.
    minor addition to player script.
    Made sibroom bg child of camera.

---
+++ What Home Means to You/Assets/Scripts/Enemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //GameObject enemy;
+    public int health = 2,
+               damage = 1,
+               timer = 0,
+               speed = 250,
+               bob = 0;
+    private Rigidbody2D rb2d;
+    private System.Random direction = new System.Random();
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    //when enemy health is x do y
+    private void Update()
+    {
+        if (health <= 0)
+            Destroy(this.gameObject);
+        if (timer++ == 100)
+        {
+            timer = 0;
+            switch (direction.Next(0, 4))
+            {
+                case 1://Right
+                    bob = 1;
+                    break;
+                case 2://Left
+                    bob = -1;
+                    break;
+                case 3://Fart
+                    bob = 0;
+                    break;
+            }
+        }
+        rb2d.velocity = new Vector2(Mathf.Lerp(rb2d.velocity.x, bob * speed * Time.deltaTime, Time.deltaTime * 10), rb2d.velocity.y);
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            health -= other.GetComponent<Player>().damage;
+        }
+    }
+}
---
+++ What Home Means to You/Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public int damage = 1,
+               Health = 10, 
+               armour = 0, 
+               shield = 0;
+    private SpriteRenderer SprtRndrr;
+    public float speed,
+                 groundCheckRadious;
+    public LayerMask isGroundedLayer;
+    private Rigidbody2D rb2d;
+    private float jumpHeight = 5,
+                  movement;
+    private bool isGrounded,
+                 toRight = true;
+   private bool[] items; //0 oldBlanket,   //1 flashLight,
+                         //2 spade,        //3 cellphone,
+                         //4 attic,        //5 GOAL,
+                         //6 slingShot,    //7 garrage,
+                         //8 baterie1,     //9 baterie2,
+                         //10baterie3,     //11umbrella,
+                        //12animalFood;
+
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        SprtRndrr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (transform.position.y <= -20 || Health <= 0) {
+            Application.LoadLevel("GameOver");
+        }
+        isGrounded = Physics2D.OverlapArea(new Vector2(transform.position.x - groundCheckRadious, transform.position.y - groundCheckRadious),
+                     new Vector2(transform.position.x + groundCheckRadious, transform.position.y - groundCheckRadious), isGroundedLayer);
+
+        movement = Mathf.Lerp(rb2d.velocity.x, Input.GetAxis("Horizontal") * speed * Time.deltaTime, Time.deltaTime * 10);
+        rb2d.velocity = new Vector2(movement, rb2d.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            rb2d.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+
+        if ((rb2d.velocity.x < 0 && toRight)||(rb2d.velocity.x > 0 && !toRight)) { 
+            toRight = !toRight;
+            SprtRndrr.flipX = !SprtRndrr.flipX;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Mob")
+        {
+           Health -= other.GetComponent<Enemy>().damage;
+        }
+    }
+
+}
+
